f51ad0cfdb459c948f34d4d77b5ac280c049ce2b
Buddy <buddyzhang@shijiebang.net>
2014-04-29 10:37:39 +0800

    Initial Commit

---
+++ SJBTreeListTableView/SJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SJBAppDelegate.h
+//  SJBTreeListTableView
+//
+//  Created by Buddy on 29/4/14.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SJBTreeListTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SJBTreeListTableView
+//
+//  Created by Buddy on 29/4/14.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SJBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SJBAppDelegate class]));
+    }
+}
