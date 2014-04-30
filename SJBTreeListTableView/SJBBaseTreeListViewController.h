94abc363219aca0813ab67eaa9780cc761d99cbf
Buddy <buddyzhang@shijiebang.net>
2014-04-30 09:45:33 +0800

    更改了spec文件，把SJBTreeViewController里面的头文件去掉了

--- SJBTreeListTableView/SJBBaseTreeListViewController.h
+++ SJBTreeListTableView/SJBBaseTreeListViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014年 apple. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface SJBBaseTreeListViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
 
 ///可以展开合并的表格
